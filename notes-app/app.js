837e4c710774e5cd67581fc05fcc4720201541e7
debughercules <debughercules@gmail.com>
2022-09-07 12:43:33 +0530

    Nodemode and till udemy 4.1

--- notes-app/app.js
+++ notes-app/app.js
@@ -9,7 +9,25 @@
 // console.log(sum)
 
 const getNotes = require('./notes')
-const msg = getNotes()
-console.log(msg)
+// const msg = getNotes()
+// console.log(msg)
 
 //3.11
+// const validator = require('validator')
+// console.log(validator.isEmail('and.com'))
+// console.log(validator.isURL('https//chattorijeebein.com'))
+
+//3.12
+const chalk = require('chalk')
+// console.log(chalk.red.bold.inverse(msg))
+
+//3.13
+// console.log(process.argv[2])
+
+const command = process.argv[2]
+
+if (command === 'add') {
+  console.log('adding note !')
+} else if (command === 'remove') {
+  console.log('removing note !')
+}
